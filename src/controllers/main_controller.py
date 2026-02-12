a29c64862fb2b96539ff053b69307d5e6b5d59b3
Zahcoder34 <Zakhriw@gmail.com>
2026-02-12 15:48:51 +0500

    test(controller) update main controller and test script

--- src/controllers/main_controller.py
+++ src/controllers/main_controller.py
@@ -1,5 +1,25 @@
 from ascender.core import Controller, Get
 
+from attp.decorators.attp_call import AttpCall
+from attp.decorators.attp_event import AttpEvent
+from attp.decorators.attp_handler import AttpErrorHandler
+from attp.decorators.attp_lifecycle import AttpLifecycle
+from attp.shared.transmitter import AttpTransmitter
+from attp.types.frame import AttpFrameDTO
+from attp.types.frames.error import IAttpErr
+
+
+class EchoRequest(AttpFrameDTO):
+    message: str
+
+
+class EchoResponse(AttpFrameDTO):
+    message: str
+
+
+class NotifyEvent(AttpFrameDTO):
+    event: str
+
 
 @Controller(
     standalone=True,
@@ -8,9 +28,36 @@ from ascender.core import Controller, Get
     providers=[],
 )
 class MainController:
-    def __init__(self):
-        ...
+    def __init__(self, transmitter: AttpTransmitter,):  
+        self.transmitter = transmitter
     
-    @Get()
-    async def main_endpoint(self):
-        return "main works!"
+    @Get("try")
+    async def try_echo(self):
+        resp = await self.transmitter.send(
+            "echo", 
+            EchoRequest(message="test"), 
+            namespace="peer-1", 
+            expected_response=EchoResponse
+        )
+        return resp
+        
+    @AttpCall("echo", namespace="peer-1")
+    async def echo(self, message: str):
+        print(message)
+        return EchoResponse(message=message)
+
+    @AttpEvent("notify")
+    async def notify(self, payload: NotifyEvent):
+        self._last_event = payload.event
+
+    @AttpErrorHandler("echo")
+    async def echo_error(self, err: IAttpErr):
+        self._last_error = err
+
+    @AttpLifecycle("connect", namespace="peer-1")
+    async def on_connect(self, payload: dict | None = None):
+        self._last_connect = payload
+
+    @AttpLifecycle("disconnect", namespace="peer-1")
+    async def on_disconnect(self, payload: dict | None = None):
+        self._last_disconnect = payload
